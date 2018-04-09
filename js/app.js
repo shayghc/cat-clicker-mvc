de05ea5393bc4ed6863227cee163f4f69c1a7b9c
Seamus Connolly <sghconnolly@gmail.com>
2018-04-09 11:59:29 +0100

    feat: Add app.js file with basic structure.

---
+++ js/app.js
@@ -0,0 +1,29 @@
+/*
+ * Controller construct
+ */
+let controller = {
+
+};
+
+
+/*
+ * View construct
+ *
+ * Two view objects are required, one for the list (which is called only once)
+ * and one for the display area which changes when a cat in the list is selected.
+ */
+let listView = {
+    // Initialise the view
+    // Create the buttons
+    // Assign the event listeners
+
+    // Render the display
+
+};
+
+let displayView = {
+    // Initialise the view: Cat name, cat image and click count
+
+    // Render the display
+
+};
