453d6f1009d7eca99b17c225f94dd10e96d5dc26
Seamus Connolly <sghconnolly@gmail.com>
2018-04-09 13:55:53 +0100

    feat: Add cat object array in model.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,64 @@
-/*
- * Controller construct
- */
-let controller = {
+'use strict';
 
+/*
+ * Model construct *************************************************************
+ * Set the cat objects array
+ */
+let model = {
+    // Set objects array here.
+    let count = 0,
+    catArray = [];
+    const catNames = ['Feynman', 'Oppenheimer', 'Frisch', 'Zazzles', 'Fermi'];
+    // Royalty free images from https://www.pexels.com
+    const catImages = ['images/catInBag.jpg', 'catInChair.jpg', 'greyCat.jpg', 'hidingCat.jpg', 'relaxedCat.jpg'];
+
+    // allCats constructor declaration.
+    class Cat {
+        constructor(name, image, count) {
+        this.name = name;
+        this.url = image;
+        this.count = count;
+        }
+    }
+
+    // Create cat objects with name, image and own counter.
+    for (let i = 0; i < catNames.length; i++) {
+        catArray[i] = new Cat(catNames[i], catImages[i], count);
+    }
 };
 
 
 /*
- * View construct
+ * Controller construct *******************************************************
+ */
+let controller = {
+ // Code here for driving the programme.
+};
+
+
+/*
+ * View construct *************************************************************
  *
  * Two view objects are required, one for the list (which is called only once)
  * and one for the display area which changes when a cat in the list is selected.
  */
 let listView = {
-    // Initialise the view
-    // Create the buttons
-    // Assign the event listeners
+    // Initialise the view.
+    // Create the buttons.
+    // Assign the event listeners.
 
-    // Render the display
+    // Render the display.
 
 };
+
 
 let displayView = {
-    // Initialise the view: Cat name, cat image and click count
+    // Initialise the view: Cat name, cat image and click count.
 
-    // Render the display
+    // Render the display.
 
 };
+
+
+// Ready?
+controller.init();
